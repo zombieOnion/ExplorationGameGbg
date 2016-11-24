ba2fc18ae4ff25bce3ce8832416beb710f3e83c5
Aleks Molchanov <aleksandr.molcanov@gmail.com>
2016-11-24 03:04:05 +0100

    fireball nöjd

--- Assets/scr_particles/FireBall.cs
+++ Assets/scr_particles/FireBall.cs
@@ -7,11 +7,17 @@ public class FireBall : MonoBehaviour {
 	public float destroyTimer = 1f;
 	private Object explosionOnce;
 	private Vector3 explosionPos;
+	private Object child1;
+	private Object child2;
+	private Object child3;
 	void Start () {
 		explosionOnce = Resources.Load("fx_explosion_once");
 		//Find the components needed in child objects for the "once" explosions
-		//fx_explosion_once = this.transform.Find("explosion_once").gameObject;
+		child1 = this.transform.Find("particle_embers").gameObject;
+		child2 = this.transform.Find("particle_fireball").gameObject;
+		child3 = this.transform.Find("particle_lensflare").gameObject;
 		//fx_sparks_once = this.transform.Find("embers_once").gameObject;
+
 	}
 	
 	// Update is called once per frame
@@ -20,8 +26,19 @@ public class FireBall : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision col){
+		//döda sin egen collider så man ej studsar mot andra projektiler o ser konstig ut
+		(gameObject.GetComponent(typeof(Collider)) as Collider).isTrigger = true;
+		//stoppa projektilen
+		moveSpeed = 0f;
+		//hämta en position som man kan sen spawna en "explosion" på
 		explosionPos = transform.position;
+		//skapa explosionen
 		Instantiate(explosionOnce, explosionPos, transform.rotation);
-		Destroy(gameObject);
+		//destroy child objects 0, 1 and 2:
+		Destroy(child1);
+		Destroy(child2);
+		Destroy(child3);
+		//destroy self:
+		Destroy(gameObject, destroyTimer);
 	}
 }
